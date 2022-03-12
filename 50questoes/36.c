e2756383c2887e39dd34dea93f3946cbe36eff8e
K4rma13 <pedrodviana@hotmail.com>
2022-03-12 12:12:55 -0500

    Added

--- 50questoes/36.c
+++ 50questoes/36.c
@@ -4,7 +4,9 @@ int comuns(int* a,int na,int* b,int nb){
 	int i,j,com=0;
 	for(i=0;i<na;i++){
 		for(j=0;j<nb;j++){
-			com++;
+			if(a[i]==b[j]){
+				com++;
+			}
 		}
 	}
 	return com;
@@ -12,6 +14,6 @@ int comuns(int* a,int na,int* b,int nb){
 
 int main(){
 	int fst[20]={4,3,7,2,2,5,32,46,13},snd[20]={5,4,2,13,5,6,8,62,1,3,63};
-	printf("Ha %d em comum", comuns(fst,9,snd,11));
+	printf("Ha %d em comum\n", comuns(fst,9,snd,11));
 	return 0;
 }
