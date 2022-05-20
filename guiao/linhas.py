1ba13d6c4d717d9875cd35281375a5c5b90af318
K4rma13 <pedrodviana@hotmail.com>
2022-05-20 13:37:41 -0400

    Final

---
+++ guiao/linhas.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python3
+
+import subprocess
+
+p = subprocess.Popen(['ls', 'code/'], stdout=subprocess.PIPE)
+files = p.communicate()[0].split(b'\n')
+count = 0
+tabsize = [len(name) for name in files]
+tabsize = max(tabsize) 
+for file in files:
+	if file:
+		with open(f'code/{file.decode()}',"r") as f:
+			data = f.read()
+		lines = len([line for line in data.split("\n") if len(line)>0])
+		count+=lines
+		print(f'{file.decode():<{tabsize+3}} {lines}')
+print(f'{"Total":<{tabsize+3}} {count}')
